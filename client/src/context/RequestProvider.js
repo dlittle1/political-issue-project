8b825e893451f1d11e039268f6c39d1d649ff257
Dylan Little <dylan.c.little@gmail.com>
2022-03-31 23:51:37 -0600

    posts showing on home page

--- client/src/context/RequestProvider.js
+++ client/src/context/RequestProvider.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { UserContext } from './UserProvider';
 import axios from 'axios';
 
@@ -31,10 +31,15 @@ export default function RequestProvider(props) {
 
   // Get popular posts
   const getPopularPosts = () => {
-    userRequestAxios
+    return userRequestAxios
       .get(`/api/posts/popular`)
-      .then((response) => console.log(response))
-      .catch((err) => console.dir(err));
+      .then((response) => response.data);
+    // const promise = userRequestAxios
+    //   .get(`/api/posts/popular`)
+    //   .then((response) => {
+    //     return response.data;
+    //   })
+    //   .catch((err) => console.dir(err));
   };
 
   // Get posts by current user
@@ -96,7 +101,7 @@ export default function RequestProvider(props) {
   };
 
   return (
-    <RequestContext.Provider value={getPopularPosts}>
+    <RequestContext.Provider value={{ getPopularPosts }}>
       {props.children}
     </RequestContext.Provider>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,49 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import axios from 'axios';
 import '../styles/home.css';
 import { RequestContext } from '../context/RequestProvider';
 
 const Home = () => {
   const requestContext = useContext(RequestContext);
+  const { getPopularPosts } = requestContext;
+  const [posts, setPosts] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getPopularPosts().then((response) => setPosts(response));
+  }, []);
+
+  useEffect(() => {
+    if (posts) {
+      setLoading(false);
+    }
+  }, [posts]);
   return (
     <div className='home-container'>
-      <div className='home-left-sidebar'>
-        <ul>
-          <li>Create New Post</li>
-          <li>View Your Posts</li>
-          <li></li>
-        </ul>
-      </div>
-      <div className='home-posts-container'>Posts</div>
-      <div className='home-right-sidebar'>tags</div>
+      {!loading && (
+        <>
+          <div className='home-left-sidebar'>
+            <ul>
+              <li>Create New Post</li>
+              <li>View Your Posts</li>
+              <li></li>
+            </ul>
+          </div>
+          <div className='home-posts-container'>
+            {posts.map((post, index) => (
+              <div key={index}>
+                <h1>{post.title}</h1>
+                <h2>{post.description}</h2>
+                <p>{post.likes.length}</p>
+                {post.tags.map((tag, index) => (
+                  <li key={index + tag}>{tag}</li>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div className='home-right-sidebar'>tags</div>
+        </>
+      )}
     </div>
   );
 };
