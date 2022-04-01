b75365fddbae18018c537dea015e070f0f5879fb
Dylan Little <dylan.c.little@gmail.com>
2022-04-01 16:49:52 -0600

    give User a username

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
