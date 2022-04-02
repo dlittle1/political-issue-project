4e11bede75ad1508fec18f848c72b9d4e77e24a9
Dylan Little <dylan.c.little@gmail.com>
2022-04-01 19:29:09 -0600

    initialize TimeAgo with defaultLocale

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import App from './App';
 import UserProvider from './context/UserProvider';
 import RequestProvider from './context/RequestProvider';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
+TimeAgo.addDefaultLocale(en);
 
 const container = document.getElementById('root');
 
