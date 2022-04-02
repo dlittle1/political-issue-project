f8215df78a8bd251d58523de87f4a3e875ffa4d3
Dylan Little <dylan.c.little@gmail.com>
2022-04-01 22:13:32 -0600

    get likes on post delete likes on post

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -11,6 +11,8 @@ const {
   updatePost,
   deletePost,
   likePost,
+  deleteLike,
+  getLikePost,
 } = postController;
 
 postRouter.route('/').get(getAllPosts).post(createPost);
@@ -19,5 +21,7 @@ postRouter.get('/new', getNewPosts);
 postRouter.get('/user', getCurrentUserPosts);
 postRouter.route('/:postId').get(getOnePost).put(updatePost).delete(deletePost);
 postRouter.put('/:postId/like', likePost);
+postRouter.get('/:postId/like', getLikePost);
+postRouter.put('/:postId/like/delete', deleteLike);
 
 module.exports = postRouter;
