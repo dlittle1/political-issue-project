2e339be1122a73c6b6a249cadbeb1b640d043d82
Dylan Little <dylan.c.little@gmail.com>
2022-04-01 19:29:47 -0600

    create left sidebar component for showing links to pages

---
+++ client/src/components/LeftSidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './componentStyles/leftSidebar.css';
+import { NavLink } from 'react-router-dom';
+const LeftSidebar = () => {
+  return (
+    <div className='left-sidebar'>
+      <div className='left-sidebar-items'>
+        <ul>
+          <NavLink to='/home/popular'>
+            <li>Home</li>
+          </NavLink>
+          <NavLink to='/home/new'>
+            <li>Newest</li>
+          </NavLink>
+          <NavLink to='/home/liked'>
+            <li>My Likes</li>
+          </NavLink>
+          <NavLink to='/home/user/posts'>
+            <li>My Posts</li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
