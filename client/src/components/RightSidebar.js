b6ee30395cbb801cfac6c079138ba9cf4c6a285f
Dylan Little <dylan.c.little@gmail.com>
2022-04-02 23:43:13 -0600

    create RightSidebar component for showing tags on homepage

---
+++ client/src/components/RightSidebar.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { RequestContext } from '../context/RequestProvider';
+import './componentStyles/rightSidebar.css';
+
+const RightSidebar = () => {
+  const context = useContext(RequestContext);
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    context.getTags().then((response) => setTags(response[0].tags));
+  });
+
+  return (
+    <div className='tags'>
+      <div className='tags-title'>Tags:</div>
+
+      <div className='tags-container'>
+        {tags.map((tag, index) => (
+          <span className='tag' key={tag + '-' + index}>
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RightSidebar;
