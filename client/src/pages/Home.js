f63eedadfb4c06434d089584684e8c6d8c4bf6f8
Dylan Little <dylan.c.little@gmail.com>
2022-04-02 23:44:24 -0600

    add rightSidebar component to homepage for displaying tags

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import '../styles/home.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMemo } from '@fortawesome/pro-solid-svg-icons';
 import LeftSidebar from '../components/LeftSidebar';
+import RightSidebar from '../components/RightSidebar';
 const Home = () => {
   return (
     <div className='home-container'>
@@ -13,7 +12,9 @@ const Home = () => {
         <div className='home-posts-container'>
           <Outlet />
         </div>
-        <div className='home-right-sidebar'>tags</div>
+        <div className='home-right-sidebar'>
+          <RightSidebar />
+        </div>
       </>
     </div>
   );
