bfe259102e94959a61c9b41fe74e6a2ce6b80082
Dylan Little <dylan.c.little@gmail.com>
2022-04-03 22:59:43 -0600

    remove unused navbar items

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { UserContext } from '../context/UserProvider';
 import './componentStyles/navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faMemo,
   faHouse,
   faUser,
   faRightFromBracket,
 } from '@fortawesome/pro-solid-svg-icons';
 
 const Navbar = () => {
+  const userContext = useContext(UserContext);
+
+  const handleSignout = () => {
+    userContext.signout();
+  };
+
   return (
     <>
       <nav className='navbar'>
-        <li className='nav-logo'>Rock the Vote</li>
-        <input type='text' className='nav-search' placeholder='search...' />
+        <li className='nav-logo'>
+          <Link to='/popular' style={{ color: '#2c2c2c' }}>
+            Rock the Vote
+          </Link>
+        </li>
+
         <ul className='nav-item-container'>
-          <li className='nav-item'>
-            <FontAwesomeIcon icon={faHouse} />
-          </li>
-          <li className='nav-item'>
-            <FontAwesomeIcon icon={faMemo} />
-          </li>
-          <li className='nav-item'>
+          {/* <li className='nav-item'>
+            <Link to='/popular' style={{ color: '#2c2c2c' }}>
+              <FontAwesomeIcon icon={faHouse} />
+            </Link>
+          </li> */}
+          {/* <li className='nav-item'>
             <FontAwesomeIcon icon={faUser} />
-          </li>
-          <li className='nav-item'>
+          </li> */}
+          <li className='nav-item' onClick={handleSignout}>
+            <p style={{ display: 'inline', marginRight: '10px' }}>Sign Out</p>
             <FontAwesomeIcon icon={faRightFromBracket} />
           </li>
         </ul>
