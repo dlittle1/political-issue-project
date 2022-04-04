fb1dfe42b9e489c6ce6356449bb44fbcad61037a
Dylan Little <dylan.c.little@gmail.com>
2022-04-03 22:57:31 -0600

    rename urls to be more cohesive

--- client/src/components/LeftSidebar.js
+++ client/src/components/LeftSidebar.js
@@ -6,17 +6,20 @@ const LeftSidebar = () => {
     <div className='left-sidebar'>
       <div className='left-sidebar-items'>
         <ul>
-          <NavLink to='/home/popular'>
+          <NavLink to='/popular'>
             <li>Home</li>
           </NavLink>
-          <NavLink to='/home/new'>
+          <NavLink to='/new'>
             <li>Newest</li>
           </NavLink>
-          <NavLink to='/home/likes'>
+          <NavLink to='/likes'>
             <li>My Likes</li>
           </NavLink>
-          <NavLink to='/home/user/posts'>
+          <NavLink to='/user/posts'>
             <li>My Posts</li>
+          </NavLink>
+          <NavLink to='/posts/new'>
+            <li style={{ marginTop: '30px' }}>Create New Post</li>
           </NavLink>
         </ul>
       </div>
